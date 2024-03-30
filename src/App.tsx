01f18c7218be7756411989abd4991d36bd3e8f57
Iris Bolocan <nexuss13@gmail.com>
2024-03-30 12:57:16 +0300

    added count function, emoji function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 
 import './App.css'
-import { Layout } from './components/layout/Layout'
+
 import Homepage from './pages/hompage/Homepage'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
--- src/components/charsCounter/CharsCounter.tsx
+++ src/components/charsCounter/CharsCounter.tsx
@@ -1,9 +1,43 @@
-import React from "react";
+import { useState } from "react";
 
 export function CharsCounter() {
+  const [inputValue, setInputValue] = useState("");
+  const [emoji, setEmoji] = useState("");
+
+  // Function to handle input change
+  const handleChange = (event: any) => {
+    setInputValue(event.target.value);
+    changeEmojy(event.target.value);
+    return event.target.value;
+  };
+
+  const countLetters = () => {
+    return inputValue.length;
+  };
+  const changeEmojy = (nameValue: any) => {
+    if (nameValue === "idan") {
+      setEmoji("😈"); // Set emoji to the desired value
+    }
+    else if (nameValue === "happy") {
+      setEmoji("😊");
+    } 
+    
+    else {
+      setEmoji(""); // Reset emoji if nameValue is not "idan"
+    }
+  };
+
   return (
     <div>
-      <input placeholder="write something"></input>
+      <input
+        type="text"
+        onChange={handleChange}
+        placeholder="write something"
+      ></input>
+      <p>Number of letters: {countLetters()} </p>
+      <p>{emoji}</p>
     </div>
   );
 }
+
+export default CharsCounter;
